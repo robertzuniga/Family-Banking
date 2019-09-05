c16cb1b0412e3a35a123512c0b43dcf0c8bb2d34
Robert Zuniga <robert@mastertheday.com>
2019-09-05 15:11:52 -0400

    update from laptop EOD

--- server/controllers/api/calculationController.js
+++ server/controllers/api/calculationController.js
@@ -3,6 +3,7 @@ const controller = require('express').Router();
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base('appfX5sQa53WlKBDg');
 
+//Create Financial Calculator records
 controller.post('/', (req,res) => {
     //req.body contains present value / rate / periods / years
 
@@ -22,6 +23,7 @@ controller.post('/', (req,res) => {
 
 })
  
+// myRetrieve a Financial Calculator record
 controller.get('/', (req,res) => {
 
   base('Financial Calculator').find('recvzk8qUIcZEXrHG', function(err, record) {
@@ -30,5 +32,32 @@ controller.get('/', (req,res) => {
   });
 
 })
+
+//my Update Financial Calculator records
+
+base('Financial Calculator').update("recvzk8qUIcZEXrHG", {
+  "User ID": 1,
+  "Present Value": 50000,
+  "Rate": 0.1,
+  "Periods": 4,
+  "Years": 5
+}, function(err, record) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(record.get('User ID'));
+});
+
+//my Delete Record
+base('Financial Calculator').destroy('recvzk8qUIcZEXrHG', function(err, deletedRecord) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Deleted record', deletedRecord.id);
+});
+OU
+
 
 module.exports = controller;
