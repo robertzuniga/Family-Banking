7cff21e7729084791f26bfe6031d4d9953101cd6
Robert Zuniga <github@mastertheday.com>
2019-09-05 23:24:42 -0400

    desktop

--- server/controllers/api/calculationController.js
+++ server/controllers/api/calculationController.js
@@ -3,35 +3,35 @@ const controller = require('express').Router();
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base('appfX5sQa53WlKBDg');
 
-//Create Financial Calculator records
-// controller.post('/', (req,res) => {
-//     //req.body contains present value / rate / periods / years
+// Create Financial Calculator records
+controller.post('/', (req,res) => {
+    //req.body contains present value / rate / periods / years
 
-//   base('Financial Calculator').create({
-//     "User ID": 1,
-//     "Present Value": 60000,
-//     "Rate": 0.1,
-//     "Periods": 4,
-//     "Years": 5
-//   }, function(err, record) {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//     console.log(record.getId());
-//   });
+  base('Financial Calculator').create({
+    "User ID": 1,
+    "Present Value": 60000,
+    "Rate": 0.1,
+    "Periods": 4,
+    "Years": 5
+  }, function(err, record) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(record.getId());
+  });
 
-// })
+})
  
 // myRetrieve a Financial Calculator record
-// controller.get('/', (req,res) => {
+controller.get('/', (req,res) => {
 
-//   base('Financial Calculator').find('recvzk8qUIcZEXrHG', function(err, record) {
-//     if (err) { console.error(err); return; }
-//     console.log('Retrieved', record.id);
-//   });
+  base('Financial Calculator').find('recvzk8qUIcZEXrHG', function(err, record) {
+    if (err) { console.error(err); return; }
+    console.log('Retrieved', record.id);
+  });
 
-// })
+})
 
 //my Update Financial Calculator records
 
@@ -49,14 +49,14 @@ var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base('appfX5sQa53WlK
 //   console.log(record.get('User ID'));
 // });
 
-//my Delete Record
-// base('Financial Calculator').destroy('recvzk8qUIcZEXrHG', function(err, deletedRecord) {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log('Deleted record', deletedRecord.id);
-// });
+// my Delete Record
+base('Financial Calculator').destroy('recvzk8qUIcZEXrHG', function(err, deletedRecord) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Deleted record', deletedRecord.id);
+});
 
 
 
