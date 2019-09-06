6328f114d0452f5e45719a924c044eb5d5b83a3e
Robert Zuniga <github@mastertheday.com>
2019-09-06 03:07:43 -0400

    desktop EOD

--- sandbox/financialCalculators/presentValue.js
+++ sandbox/financialCalculators/presentValue.js
@@ -2,41 +2,41 @@
 
 
 
-    // let presentValue = 1000;
-    // let rate = 0.05;
-    // let periods = 12;
-    // let years = 10;
+// let presentValue = 1000;
+// let rate = 0.05;
+// let periods = 12;
+// let years = 10;
 
 
 
-    function futureValue(presentValue,rate,periods,years) {
-        let fv = presentValue * Math.pow((1 + rate / periods), (periods * years));
-        return fv;   // The function returns the product of p1 and p2
-      };
+function futureValue(presentValue, rate, periods, years) {
+  let fv = presentValue * Math.pow((1 + rate / periods), (periods * years));
+  return fv;
+};
 
-    // console.log("presentValue ==>", presentValue);
+// console.log("presentValue ==>", presentValue);
 
-    // let futureValue = presentValue * Math.pow((1 + rate / periods), (periods * years));
+// let futureValue = presentValue * Math.pow((1 + rate / periods), (periods * years));
 
-    // console.log("futureValue ==>", futureValue);
+// console.log("futureValue ==>", futureValue);
 
 
 
-    // for (let i = 1; i <= years; i++) {
+// for (let i = 1; i <= years; i++) {
 
-    //     let fv = 0;
-    //     fv = presentValue * Math.pow((1 + rate / periods), (periods * i));
-    //     console.log("year ==> " + i + " fv ==> " + fv);
-    // }
+//     let fv = 0;
+//     fv = presentValue * Math.pow((1 + rate / periods), (periods * i));
+//     console.log("year ==> " + i + " fv ==> " + fv);
+// }
 
-    // console.log("futureValue ==>", futureValue);
+// console.log("futureValue ==>", futureValue);
 
-    function presentValue(futureValue,rate,periods,years) {
-        let numerator = futureValue;
-        let denominator = Math.pow((1 + rate / periods), (periods * years));
-        pv = numerator / denominator;
-        return pv;   // The function returns the product of p1 and p2
-      };
+function presentValue(futureValue, rate, periods, years) {
+  let numerator = futureValue;
+  let denominator = Math.pow((1 + rate / periods), (periods * years));
+  pv = numerator / denominator;
+  return pv;
+};
 
     // let numerator = futureValue;
     // let denominator = Math.pow((1 + rate / periods), (periods * years));
